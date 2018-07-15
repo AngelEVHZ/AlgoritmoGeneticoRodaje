a9d64015f9cc1eccf54c430ce635099a3846488f
Julieta <julietaescobar.jeg@gmail.com>
2018-07-14 23:47:44 -0500

    Vistas

--- AlgoritmoGeneticoRodaje/src/algoritmogeneticorodaje/FXMLDocumentController.java
+++ AlgoritmoGeneticoRodaje/src/algoritmogeneticorodaje/FXMLDocumentController.java
@@ -5,31 +5,45 @@
  */
 package algoritmogeneticorodaje;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Valenzuela
  */
 public class FXMLDocumentController implements Initializable {
-    
+    Stage stage;
     @FXML
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void abrirVentana(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Recursos.fxml"));
+        Parent root = (Parent)loader.load();
+        RecursosController controller = (RecursosController)loader.getController();
+        controller.setStage(this.stage);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        this.stage = new Stage();
     }    
+    
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
     
 }
